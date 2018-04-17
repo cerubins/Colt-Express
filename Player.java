58e44f4909f09a6007ac771d9877ac314d38196b
Gabe Rumsey <37911844+MalevolentCrab@users.noreply.github.com>
2018-04-17 13:17:33 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,5 +3,7 @@ public class Player {
 	private int money;
 	private int bullets = 6;
 	private Stack<ActionCard> cardDrawPile = new Stack<ActionCard>();
-	public Platform currentPlat;
+	
+	public TrainCar currentCar;
+	public int currentLevel;
 }
