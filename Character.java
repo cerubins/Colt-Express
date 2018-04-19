6aa316dddad4f8204c33af780ca066f4d3b59171
ashutoshraiofficial <ashutoshraiofficial@gmail.com>
2018-04-19 13:30:43 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -9,6 +9,7 @@ public class Character extends Player{
 	private ArrayList<Bag> bags = new ArrayList<Bag>();
 	private ArrayList<Ruby> rubies = new ArrayList<Ruby>();
 	private ArrayList<LockBox> lockboxes = new ArrayList<LockBox>();
+	private ArrayList <ActionCard> cards = new ArrayList <ActionCard> ();
 	
 	public Character(String n, TrainCar c, int le){
 		currentCar = c;
@@ -40,21 +41,41 @@ public class Character extends Player{
 	
 	public void addBags(Bag b){
 		bags.add(b);
-	{
+	}
 		
 	public void addBags(Ruby r){
 		rubies.add(b);
-	{
+	}
 	
 	public Bag removeBag(){
 		Random random = new Random();
 		int index = random.nextInt(player.getBags().size());
 		
 		return bags.get(index);
-	{
+	}
 		
 	public void addLockbox(LockBox l){
 		lockboxes.add(b);
-	{
+	}
+	
+	
+	
+	public void setCards (ArrayList <ActionCard> cs) {
+		
+		cards = cs;
+		
+	}
+	
+	public void clearCards () {
+		
+		cards.clear ();
+		
+	}
+	
+	public ActionCard getCard (int i) {
+		
+		return cards.remove (i);
+		
+	}
 		
 }
