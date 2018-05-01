a12155d4fa1f4b449fc91d73e8205d55d1051d06
ashutoshraiofficial <ashutoshraiofficial@gmail.com>
2018-05-01 13:39:52 -0500

    Update RoundCardSelector.java

--- RoundCardSelector.java
+++ RoundCardSelector.java
@@ -8,25 +8,25 @@ public class RoundCardSelector {
 		
 		ArrayList <RoundCard> a = new ArrayList <RoundCard> ();
 		
-		a.add (new RoundCard ("aloneInCar:StealOneBag", "1101", 0, true));
+		a.add (new RoundCard ("aloneInCar:StealOneBag", "1 1 0 1", 0, true));
 		
-		a.add (new RoundCard ("atopCarAndMarshall:DropBag", "1101", 0, true));
+		a.add (new RoundCard ("atopCarAndMarshall:DropBag", "1 1 0 1", 0, true));
 		
-		a.add (new RoundCard ("onOrInEngine:get250Cash", "1101", 0, true));
+		a.add (new RoundCard ("onOrInEngine:get250Cash", "1 1 0 1", 0, true));
 		
-		a.add (new RoundCard ("sheriffDropsNewLockBox", "10111", 5, false));
+		a.add (new RoundCard ("sheriffDropsNewLockBox", "1 0 1 1 1", 5, false));
 		
-		a.add (new RoundCard ("allPeopleAtopTrainMoveToCaboose", "1011", 4, false));
+		a.add (new RoundCard ("allPeopleAtopTrainMoveToCaboose", "1 0 1 1", 4, false));
 		
-		a.add (new RoundCard ("nothing", "10101", 5, false));
+		a.add (new RoundCard ("nothing", "1 0 1 0 1", 5, false));
 		
-		a.add (new RoundCard ("nothing", "1111", 4, false));
+		a.add (new RoundCard ("nothing", "1 1 1 1", 4, false));
 		
-		a.add (new RoundCard ("everyoneAtopMovesTowardsEngine1Space", "1111", 4, false));
+		a.add (new RoundCard ("everyoneAtopMovesTowardsEngine1Space", "1 1 1 1", 4, false));
 		
-		a.add (new RoundCard ("everyoneTakes1Damage", "11011", 5, false));
+		a.add (new RoundCard ("everyoneTakes1Damage", "1 1 0 1 1", 5, false));
 		
-		a.add (new RoundCard ("sheriffShootsAnyoneAboveHisCar", "1102", 4, false));
+		a.add (new RoundCard ("sheriffShootsAnyoneAboveHisCar", "1 1 0 2", 4, false));
 		
 		Collections.shuffle (a);
 		
