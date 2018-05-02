11e0d73479aeebe0c5a398d29618008d608b4975
nico-molina <37911914+nico-molina@users.noreply.github.com>
2018-05-02 14:21:43 -0500

    Add files via upload

---
+++ InventoryTwo.java
@@ -0,0 +1,18 @@
+
+public class InventoryTwo 
+{
+	private String str;
+	
+	public InventoryTwo()
+	{
+		str = "";
+	}
+	public InventoryTwo(String s)
+	{
+		str = s;
+	}
+	public String getType()
+	{
+		return str;
+	}
+}
