c7c6ae643f5243930640011debbd685b3a820396
Gabe Rumsey <37911844+MalevolentCrab@users.noreply.github.com>
2018-05-11 13:11:29 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -12,96 +12,16 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-
-	//"src/images/BG1.png" - correct image pathing minus the ending
-public class Game extends JPanel implements ActionListener, KeyListener{
-	
-	private static final long serialVersionUID = 1L;
-	
-	Timer gameLoopTimer;
-	private Character p1, p2, p3, p4;
-	private Image bg, trainImage;
-	private boolean choice1, choice2, choice3, choice4, choice5;
-	public JFrame a;
-	public JPanel pane;
-	public int bgx = -960;
-	public char choice = 'z';
-	
-	public Game(JFrame j) throws IOException{
-		setFocusable(true);
-		a=j;
-		pane = new JPanel();
-		
-		a.add(pane);
-		
-		pane.addKeyListener(this);
-		pane.addNotify();
-
-		gameLoopTimer = new Timer(10, this);
-		gameLoopTimer.start();
-		
-		
-		
-		bg = ImageIO.read(new File("src/images/BG1.png"));
-		trainImage = ImageIO.read(new File("src/images/trainImage.png"));
-	}
-	
-	public void addNotify(){
-		super.addNotify();
-		requestFocus();
-	}
-	
-	public void paint(Graphics g){
-		super.paint(g);
-		Graphics2D g2d = (Graphics2D) g;
-		
-		
-		//DRAWING BACKGROUND
-		if(bgx>=0)bgx = -960;
-		g2d.drawImage(bg, bgx+=4, 0, getWidth()*2, getHeight(), this); 
-		
-		//DRAWING TRAIN
-		g2d.drawImage(trainImage, 0, getHeight()-470,trainImage.getWidth(this) , trainImage.getHeight(this), this);
-		
-		switch(choice){
-		case 'e':
-			System.out.println("KILL ME NOW");
-		}
-	}
-	
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e){
-
-		repaint();
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		choice = e.getKeyChar();
-	}
-}
-
-class MyPanel extends JPanel implements KeyListener {
+class Game extends JPanel implements KeyListener {
     private char c = 'e';
-
-    public MyPanel() {
+    private Image bg, trainImage;
+    
+    public Game() throws IOException {
         this.setPreferredSize(new Dimension(500, 500));
         addKeyListener(this);
+        
+		bg = ImageIO.read(new File("src/images/BG1.png"));
+		trainImage = ImageIO.read(new File("src/images/trainImage.png"));
     }
 
     public void addNotify() {
@@ -110,8 +30,8 @@ class MyPanel extends JPanel implements KeyListener {
     }
 
     public void paintComponent(Graphics g) {
-        g.clearRect(0, 0, getWidth(), getHeight());
-        g.drawString("the key that pressed is " + c, 250, 250);
+    	g.drawImage(bg, 0, 0, this);
+    	g.drawImage(trainImage, 0, bg.getHeight(this) - 470, this);
     }
 
     public void keyPressed(KeyEvent e) { }
@@ -121,11 +41,13 @@ class MyPanel extends JPanel implements KeyListener {
         repaint();
     }
 
-    public static void main(String[] s) {
+    public static void main(String[] s) throws IOException {
         JFrame f = new JFrame();
-        f.getContentPane().add(new MyPanel());
+        f.getContentPane().add(new Game());
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setPreferredSize(new Dimension(1920, 1080));
         f.pack();
         f.setVisible(true);
+        
     }
 }
